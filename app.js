8d18b87b682aaabd8409fea848ee9307d319bd35
nguyettuhan213 <117661080+nguyettuhan213@users.noreply.github.com>
2023-01-23 22:35:25 +0700

    a

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const port =  5500;
+// const bodyParser = require('body-parser');
+const port = process.env.PORT || 5500;
 const apiPath = '/api/';
+const apiRouter = require('./routes/items.route')
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -11,16 +12,12 @@ app.use(express.urlencoded());
 // website
 app.use(express.static('client'));
 
-app.get('/test', (req, res)=>{
-	res.send('1234567');
-})
-
 // routers
-app.use(apiPath + 'items', require('./routes/items.route'));
+app.use(apiPath + 'items', apiRouter);
 // app.use(apiPath + 'products', require('./routes/products.route'));
 // app.use(apiPath + 'upload', require('./routes/upload.route'));
 
 app.listen(port, function () {
-	const host = '127.0.0.1';
+	const host = 'localhost';
 	console.log('Example app listening at http://%s:%s', host, port);
 });
