e5c604aa533ae8a7f26066b7743188951f881fec
duongvanhung <chomotlat1234@gmail.com>
2023-01-26 17:26:39 +0700

    kêt noi sql

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,16 +1,54 @@
 const Item = require('../models/item.model');
-const people = require('../assets/items.json');
+const items = require('../assets/items.json');
+const db = require('../database');
 
 class itemController {
 	get(req, res) {
-		return res
-			.status(200)
-            .json('hello');
+		db.connectDB()
+			.then((connection) => {
+
+				connection.query(
+					'SELECT * FROM fashionshop',
+					function (err, data, fields) {
+						console.log('data',data);
+						db.closeDB(connection);
+						return res.status(200).json(data);
+					}
+				);
+			})
+			.catch((error) => {
+				console.log('Db not connected successfully', error);
+				return res
+					.status(200)
+					.json({ result: `Không thể kết nối Database` });
+			});
 	}
 
 	post(req, res) {
-		return res.status(200).json({ result: `Chào bạn abc` });
+		const name = req.body.name;
+		// console.log('username', username);
+		// console.log('password', password);
+
+		db.connectDB()
+			.then((connection) => {
+				console.log('connected successfully');
+				connection.query(
+					`INSERT INTO fashionshop(productname,typeproduct, price, sell, brand,size, image) VALUES('${name}', '', NaN,NaN, '', '','')`,
+					function (err, data, fields) {
+					
+						db.closeDB(connection);
+						return res.status(200).json({ result: `Thành công` });
+					}
+				);
+			})
+			.catch((error) => {
+				console.log('Db not connected successfully', error);
+				return res
+					.status(200)
+					.json({ result: `Không thể kết nối Database` });
+			});
 	}
 }
+	
 
 module.exports = new itemController();
