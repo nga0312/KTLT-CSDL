2940f2f5be8fc49e6ecb3656e7da03b7f6935d84
duongvanhung <chomotlat1234@gmail.com>
2023-01-26 22:32:49 +0700

    kết nối my sql

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -8,7 +8,7 @@ class itemController {
 			.then((connection) => {
 
 				connection.query(
-					'SELECT * FROM fashionshop',
+					'SELECT * FROM fashionshop.product',
 					function (err, data, fields) {
 						console.log('data',data);
 						db.closeDB(connection);
@@ -26,6 +26,11 @@ class itemController {
 
 	post(req, res) {
 		const name = req.body.name;
+		const intro = req.body.intro;
+		const type = req.body.type;
+		const price =req.body.price;
+		
+
 		// console.log('username', username);
 		// console.log('password', password);
 
@@ -33,11 +38,12 @@ class itemController {
 			.then((connection) => {
 				console.log('connected successfully');
 				connection.query(
-					`INSERT INTO fashionshop(productname,typeproduct, price, sell, brand,size, image) VALUES('${name}', '', NaN,NaN, '', '','')`,
+					`INSERT INTO fashionshop.product(productname,intro,typeproduct, price, sell, brand,size, image) VALUES('${name}','${intro}', '${type}', '${price}',0, '', '','')`,
+					// type, price, sell=0, brand= rong, size, image=''
 					function (err, data, fields) {
 					
 						db.closeDB(connection);
-						return res.status(200).json({ result: `Thành công` });
+						return res.status(200).json({ result: `data` });
 					}
 				);
 			})
