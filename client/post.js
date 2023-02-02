f11fa5c6999a5645756c99d080027e9420967774
nguyettuhan213 <117661080+nguyettuhan213@users.noreply.github.com>
2023-02-02 16:50:41 +0700

    fillter

--- client/post.js
+++ client/post.js
@@ -16,7 +16,7 @@ async function fetchPosts(){
 }
 
 
-function listsPosts(postContainerElementId) {
+function listsPosts(postContainerElementId, filter) {
     const postContainerElement = document.getElementById(postContainerElementId);
 
     if(!postContainerElement){
@@ -30,20 +30,23 @@ function listsPosts(postContainerElementId) {
         }
 
         for( const post of posts){
-            postContainerElement.appendChild(postElement(post));
+            postContainerElement.appendChild(postElement(post, filter));
         }
     }).catch(e =>{
         console.log('error', e);
     })
 }
 
-function postElement(post){
+function postElement(post, filter){
 
     console.log('post',post)
     const searchParams = new URLSearchParams(post);
     const queryString = searchParams.toString();
     const query = 'detalItem.html?' + queryString;
 
+    const divClass = document.createElement('div');
+    divClass.style.cssText = "display: block; visibility: visible;"
+    
     const anchorElement = document.createElement('a');
     anchorElement.setAttribute('href', `${apiUrl}/${query}`);
     anchorElement.setAttribute('target', '_black');
@@ -66,12 +69,52 @@ function postElement(post){
     const postIntroElement = document.createElement('h6');
     postIntroElement.innerText = post.intro;
 
-
+    divClass.appendChild(anchorElement);
     anchorElement.appendChild(div);
     div.appendChild(postImgElement);
     div.appendChild(div2);
     div2.appendChild(postTitleElement);
     div2.appendChild(postIntroElement);
 
-    return anchorElement;
+    // const hasItems = true;
+
+    if(filter !== 'ALL'){
+        const hasItems = check(post, filter);
+        console.log(hasItems);
+        if(hasItems === false){
+            divClass.style.cssText = "display: none; visibility: visible;"
+        }
+    }
+
+    return divClass;
+}
+
+function filterItems(postContainerElementId){
+    const btns = document.querySelectorAll('.btn')
+    console.log(btns)
+
+    let filter;
+    // console.log('filter1',filter)
+    // listsPosts(postContainerElementId, filter)
+    for (let i = 0;i <btns.length; i++){
+        btns[i].addEventListener('click', (e)=>{
+            e.preventDefault();
+            filter = e.target.dataset.filter;
+            // filter = e.target.dataset.filter;
+            listsPosts(postContainerElementId, filter)
+            console.log('filter2',filter)
+
+
+        })
+    }
+}
+
+function check(post, filter){
+    var check = false;
+    for (let key in post){
+        if(post[key] == filter) {
+            check = true;
+        }
+    }
+    return check;
 }
